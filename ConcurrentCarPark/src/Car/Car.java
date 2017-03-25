be752227a7b2e3b1bfc881b91758e356ad182b95
Sean Anglim <sean.anglim4@mail.dcu.ie>
2017-03-25 18:35:53 +0000

    TODO

--- ConcurrentCarPark/src/Car/Car.java
+++ ConcurrentCarPark/src/Car/Car.java
@@ -54,7 +54,8 @@ public abstract class Car implements Runnable {
             // CARPARK
             LOGGER.info("Car is entering the carpark -" + Thread.currentThread().getId());
             //carpark.enterCarpark();
-
+            // TODO: Park the car
+            
 
             // PARKED
             // TODO: Stay here for duration of 'stay_time'
