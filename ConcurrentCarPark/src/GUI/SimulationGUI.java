84bbf8519f67b36dbe29b0616fabaf07dc7a17a0
ciaran callaghan <ciaran.callaghan8@mail.dcu.ie>
2017-03-27 11:28:52 +0100

    remove resizable gui

--- ConcurrentCarPark/src/GUI/SimulationGUI.java
+++ ConcurrentCarPark/src/GUI/SimulationGUI.java
@@ -68,8 +68,7 @@ public class SimulationGUI extends JFrame {
         int x = (int) ((dimension.getWidth() - this.getWidth()) / 2);
         int y = (int) ((dimension.getHeight() - this.getHeight()) / 2);
         this.setLocation(x, y);
-
-        setResizable(false);
+        
         this.setVisible(true);
 
         this.updater = new GUIupdater(
